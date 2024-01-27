af73e11419e713c4c7e65d65e713868d742a4d6e
Labenvenutti <laerciobenvenutti@gmail.com>
2024-01-27 13:07:18 -0300

    Desenvolvimento da pagina de Login

---
+++ backend/dropdown/dropdown.js
@@ -0,0 +1,18 @@
+// JavaScript (script.js)
+function toggleDropdown() {
+    var dropdown = document.getElementById("myDropdown");
+    dropdown.classList.toggle("show");
+}
+
+// Fechar o dropdown se o usuário clicar fora dele
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
