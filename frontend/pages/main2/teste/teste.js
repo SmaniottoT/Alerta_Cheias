6396b89f182c3a44dbabcd0c0d09e8dbed164b75
Labenvenutti <laerciobenvenutti@gmail.com>
2024-02-15 17:52:24 -0300

    Desenvolvimento main2

---
+++ frontend/pages/main2/teste/teste.js
@@ -0,0 +1,4 @@
+document.getElementById('ativarHover').addEventListener('click', function() {
+  const elementoHover = document.getElementById('elemento');
+  elementoHover.classList.add('mudar');
+});
