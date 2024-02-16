041d008787b66920d1f6d09a7558cef7b9403bbb
Tamires Smaniotto <smaniotto.t@gmail.com>
2024-02-15 21:26:57 -0300

    página de login funcionando

--- frontend/src/login.ts
+++ frontend/src/login.ts
@@ -1,16 +1,30 @@
 import { user } from "./apiUser";
 
-document
-  .getElementById("form")
-  ?.addEventListener("submit", async (event: SubmitEvent) => {
-    event.preventDefault();
-    console.log('ok');
-    
-    const credentials = await user.post("/login", {
+
+async function login(event: Event) {
+  event.preventDefault();
+  try {
+    const credentials: any = await user.post("/login", {
       username: (document.getElementById("username") as HTMLInputElement).value,
       password: (document.getElementById("password") as HTMLInputElement).value,
     });
+    console.log(credentials);
 
-    localStorage.setItem("token", credentials.data.token);
-    window.location.replace("/pages/main/main.html");
-  });
+    if (credentials.response?.data.error) {
+      throw new Error(credentials.response.data.error);
+    }
+    window.location.replace("../../pages/main/main.html");
+  } catch (error) {
+    // alert(`Error: ${error.message}`);
+    // transformar o alert em um document.elementHtml
+    document.getElementById("error").innerText = error.message;
+    document.getElementById("error").style.display = "block";
+    document
+      .getElementById("username")
+      .addEventListener("click", (event: Event) => {
+        document.getElementById("error").style.display = "none";
+      });
+  }
+}
+
+document.getElementById("Login_Button")?.addEventListener("click", login);
